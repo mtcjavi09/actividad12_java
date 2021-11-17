85766db4368ea3af525814577cf10840697f8c15
MTC <mashjov13@outlook.com>
2021-11-16 20:46:50 -0600

    commit 5 programa 11

--- src/main/java/addressBook.java
+++ src/main/java/addressBook.java
@@ -7,7 +7,9 @@
 
 //Se importan las librerías necesarias para el correcto funcionamiento de la clase
 import java.util.HashMap; //Para crear el Hash Map que guardará el contacto
+import java.time.LocalDate; //Para poder guardar la fecha de creación del contacto
 import java.io.*; //Para manejar las excepciones
+import java.time.LocalTime; //Para poder guardar la hora de creación del contacto
 import com.csvreader.*; //Para generar y leer el archivo
 import javax.swing.JOptionPane; //Para mandar mensajes fuera de la consola
 
@@ -37,7 +39,7 @@ public class addressBook
             CsvReader reader = new CsvReader(ARCHIVO);
             
             //Se crea un nuevo HashMap para controlar el ciclo while
-            HashMap<String, contact> archivo = new HashMap <String, contact>();
+            HashMap<String, contact> archivo = new HashMap <>();
             
             //Se crea un ciclo while para guardar en el HashMap los registros encontrados
             while(reader.readRecord())
@@ -45,9 +47,11 @@ public class addressBook
                 //Se guardan los datos en las variables para el HashMap
                 String name = reader.get(0);
                 String phone = reader.get(1);
+                String date = reader.get(2);
+                String hour = reader.get(3);
 
                 //Se crea el objeto con los datos necesarios
-                contact contacto = new contact(name,phone);
+                contact contacto = new contact(name,phone,date,hour);
 
                 //Se guarda el registro en el HashMap
                 archivo.put(phone, contacto);
@@ -62,13 +66,15 @@ public class addressBook
             //Se usa un ciclo for para recorrer el HashMap
             for(Object x : archivo.values())
             {
-                //Se convierte el objeto x al objeto y
-                contact y = (contact) x;
-                //Me muestran los datos en consola
-                System.out.print("Nombre: " + y.getName() + "; ");
-                System.out.println("Telefono: " + y.getPhone());
-                //Se agrega una línea para mejor lectura
-                System.out.println();
+                 //Se convierte el objeto x al objeto y
+                 contact y = (contact) x;
+                 //Me muestran los datos en consola
+                 System.out.print("Nombre: " + y.getName() + "; ");
+                 System.out.println("Telefono: " + y.getPhone());
+                 System.out.print("Fecha de creación: "+ y.getDate() + "; ");
+                 System.out.println("Hora de creación: " + y.getHour());
+                 //Se agrega una línea para mejor lectura
+                 System.out.println();
             }
         }
         //Capta cualquier excepción que surga durante la ejecución
@@ -103,6 +109,8 @@ public class addressBook
             //Se escriben las columnas para reconocer los datos guardados
             writer.write("Contacto");
             writer.write("Teléfono");
+            writer.write("Fecha de creación");
+            writer.write("Hora de creación");
             //Deja de escribir en la misma fila
             writer.endRecord();
             
@@ -117,6 +125,10 @@ public class addressBook
                 writer.write(y.getName());
                 //Se escribe el teléfono
                 writer.write(y.getPhone());
+                //Se escribe la fecha
+                writer.write(y.getDate());
+                //Se escribe la hora
+                writer.write(y.getHour());
                 //Deja de escribie en la misma fila
                 writer.endRecord();
             }
@@ -141,7 +153,8 @@ public class addressBook
         {
            //Se le notifica al usuario que se mostrarán todos los contactos
            System.out.println("Los contactos guardados en el HashMap son:");
-           
+           //Se agrega una línea para mejor lectura
+           System.out.println();
            //Se usa un ciclo for para recorrer el HashMap
            for(Object x : contactos.values())
            {
@@ -150,6 +163,8 @@ public class addressBook
                 //Me muestran los datos en consola
                 System.out.print("Nombre: " + y.getName() + "; ");
                 System.out.println("Telefono: " + y.getPhone());
+                System.out.print("Fecha de creación: "+ y.getDate() + "; ");
+                System.out.println("Hora de creación: " + y.getHour());
                 //Se agrega una línea para mejor lectura
                 System.out.println();
            }
@@ -168,6 +183,8 @@ public class addressBook
         //Variables necesarias para el método
         String nombre; //Guarda el nombre del contacto
         String telefono; //Guarda el teléfono del contacto
+        String fecha; //Guarda la fecha de creación del contacto
+        String hora; //Guarda la hora de creación del contacto
         int intentos = 3; //Especifica el número de equivocaciones permitidas
 
         //Se especifica el manejo de excepciones try ... catch
@@ -177,8 +194,8 @@ public class addressBook
             //Se repiten las siguientes instrucciones hasta completar el número de contactos deseados
             for (int x = 0; x < cantidadContactos; x++)
             {
-                //Se pide al usuario el nombre de contacto, y se guarda en mayúsculas para homogenizar el formato
-                nombre = JOptionPane.showInputDialog("Ingresa el nombre de tu contacto:").toUpperCase();
+                //Se pide al usuario el nombre de contacto
+                nombre = JOptionPane.showInputDialog("Ingresa el nombre de tu contacto:");
                 
                 //Se crea el ciclo para pedir el número al usuario y asegurar que cumpla con 10 dígitos
                 do
@@ -204,9 +221,17 @@ public class addressBook
                 //Si el usuario se acabó sus intentos disponibles, lanza una excepción
                 if (intentos == 0)
                 {throw new Exception("La cantidad de errores permitida ha sido superada.");}
+
+                //Se guarda la fecha exacta de creación del contacto
+                //Al final se convierte a string para su guardado en el objeto
+                fecha = LocalDate.now().toString();
+                
+                //Se guarda la hora exacta de creación del contacto
+                //Al final se convierte a string para su guardado en el objeto
+                hora = LocalTime.now().toString();
                 
                 //Se crea el objeto de tipo contact con los datos ingresados
-                contact contact = new contact(nombre, telefono);
+                contact contact = new contact(nombre, telefono, fecha, hora);
                 
                 //Se guarda el objeto en el HashMap, repitiendo el teléfono como la llave
                 contactos.put(telefono, contact);
@@ -236,7 +261,7 @@ public class addressBook
             contact elimina = contactos.get(telefono);
             //Se pide confirmación del usuario para eliminar el contacto
             int confirma = Integer.parseInt(JOptionPane.showInputDialog("¿Estás seguro de eliminar el siguiente contacto?\n"
-                    + elimina.getName() + "; " + elimina.getPhone() + "\n[1] SI\n[2] NO\n"));
+                    + elimina.getName() + "; " + elimina.getPhone() + "\n[1] SI\n [2] NO\n"));
             //Se decide la acción a realizar a partir de la confirmación a partir de la decisión del usuario
             //Se usa un switch ... case para dicha acción
             switch(confirma)
@@ -281,8 +306,8 @@ public class addressBook
             //Se pide el número de teléfono del contacto que quiere modificar el usuario
             String telefono = JOptionPane.showInputDialog("Ingresa el teléfono del contacto"
                     + " que deseas modificar");
-            //Se pide al usuario el nuevo nombre de contacto, y se guarda en mayúsculas para homogenizar el formato
-            String nombre = JOptionPane.showInputDialog("Ingresa el nuevo nombre de tu contacto:").toUpperCase();
+            //Se pide al usuario el nuevo nombre de contacto
+            String nombre = JOptionPane.showInputDialog("Ingresa el nuevo nombre de tu contacto:");
 
             //Se crea el ciclo para pedir el nuevo número al usuario y asegurar que cumpla con 10 dígitos
             do
@@ -309,8 +334,16 @@ public class addressBook
             if (intentos == 0)
             {throw new Exception("La cantidad de errores permitida ha sido superada.");}
 
+            //Se guarda la fecha exacta de modificación del contacto
+            //Al final se convierte a string para su guardado en el objeto
+            String fecha = LocalDate.now().toString();
+
+            //Se guarda la hora exacta de modificación del contacto
+            //Al final se convierte a string para su guardado en el objeto
+            String hora = LocalTime.now().toString();
+            
             //Se crea el objeto de tipo contact con los nuevos datos ingresados
-            contact contact = new contact(nombre, telefono);
+            contact contact = new contact(nombre, telefono, fecha, hora);
 
             //Se realizan diferentes acciones dependiendo del número telefónico
             //Si el teléfono es igual al que tenía en el registro
--- src/main/java/contact.java
+++ src/main/java/contact.java
@@ -10,17 +10,26 @@ public class contact
     //Atributos de la clase
     private String name; //Guarda el nombre del contacto
     private String phone; //Guarda el teléfono del contacto
+    private String date; //Guarda la fecha de creación del contacto
+    private String hour; //Guarda la hora de creación del contacto
     
     //Métodos get y set para los atributos privados
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
     public String getPhone() {return phone;}
     public void setPhone(String phone) {this.phone = phone;}
+    public String getDate() {return date;}
+    public void setDate(String date) {this.date = date;}
+    public String getHour() {return hour;}
+    public void setHour(String hour) {this.hour = hour;}
+    
     
     //Constructor para creación del objeto usando Hash Map
-    public contact(String name, String phone) 
+    public contact(String name, String phone, String date, String hour) 
     {
         this.name = name;
         this.phone = phone;
+        this.date = date;
+        this.hour = hour;
     }  
 }
